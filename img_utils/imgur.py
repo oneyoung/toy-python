606d8f3cd8ddc84ace86b0af307c303f2d241cdc
Oneyoung@Acer4710 <guowangyang@gmail.com>
2013-03-21 09:29:24 +0800

    [img_utils] add attach_file to POST file

--- img_utils/imgur.py
+++ img_utils/imgur.py
@@ -5,6 +5,32 @@ import base64
 CLIENT_ID = 'd968b2b8df7f127'
 
 
+def randomstring(length):
+    import random
+    import string
+    return ''.join(random.choice(string.letters) for i in range(length))
+
+
+def attach_file(fpath):
+    import mimetypes
+    from os.path import basename
+    mtype, encoding = mimetypes.guess_type(fpath)
+    boundary = randomstring(10)
+    separator = '--%s' % boundary
+    lines = [
+        'Content-Type: multipart/form-data; boundary=%s\n' % boundary,
+        separator,
+        'Content-Disposition: form-data; name="type"',
+        'file',
+        separator,
+        'Content-Disposition: form-data; name="image"; filename="%s"' % basename(fpath),
+        'Content-type: %s' % mtype,
+        open(fpath, 'rb').read(),
+        '--%s--' % boundary,
+    ]
+    return '\n'.join(lines)
+
+
 def upload(image):
     f = open(image, 'rb')
     img_data = base64.b64encode(f.read())
