d22268de342d74fd1add1177c78d0b9b2877e42a
Wayne <guowangyang@gmail.com>
2013-03-21 16:12:25 +0800

    [img_utils] use requests module to post file

--- img_utils/imgur.py
+++ img_utils/imgur.py
@@ -1,53 +1,24 @@
-import json
-import urllib2
-import base64
-
 CLIENT_ID = 'd968b2b8df7f127'
 
 
-def randomstring(length):
-    import random
-    import string
-    return ''.join(random.choice(string.letters) for i in range(length))
-
-
-def attach_file(fpath):
-    import mimetypes
-    from os.path import basename
-    mtype, encoding = mimetypes.guess_type(fpath)
-    boundary = randomstring(10)
-    separator = '--%s' % boundary
-    lines = [
-        'Content-Type: multipart/form-data; boundary=%s\n' % boundary,
-        separator,
-        'Content-Disposition: form-data; name="type"',
-        'file',
-        separator,
-        'Content-Disposition: form-data; name="image"; filename="%s"' % basename(fpath),
-        'Content-type: %s' % mtype,
-        open(fpath, 'rb').read(),
-        '--%s--' % boundary,
-    ]
-    return '\n'.join(lines)
-
-
 def upload(image):
-    f = open(image, 'rb')
-    img_data = base64.b64encode(f.read())
-    img_data.strip()
-    data = 'image=%s&type=base64' % img_data
-
+    try:
+        import requests
+    except ImportError:
+        print "moudle requests not found"
+        print "try 'pip install requests' to install"
+        raise ImportError
     url = "https://api.imgur.com/3/upload"
-    req = urllib2.Request(url)
-    req.add_header('Authorization', 'Client-ID %s' % CLIENT_ID)
-    req.add_data(data)
+    headers = {'Authorization': 'Client-ID %s' % CLIENT_ID}
+    files = {'image': open(image, 'rb').read()}
 
-    response = urllib2.urlopen(req)
-    result = json.loads(response.read())
+    response = requests.post(url, headers=headers, files=files, verify=False)
+    result = response.json()
     if result.get('status'):
         link = result.get('data').get('link')
         return link
 
 
 if __name__ == "__main__":
-    print upload('/tmp/img.jpg')
+    #print upload('/tmp/img.jpg')
+    pass
